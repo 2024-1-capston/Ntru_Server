50e6d7bcfcd627a94f1e9b3fd9c685b7b4b5b5b9
DongyoungKim <kairos.hk0912@gmail.com>
2024-05-09 19:46:53 +0900

    Update ntru_server.py

--- ntru_server.py
+++ ntru_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import subprocess
+import base64
 
 app = Flask(__name__)
 
@@ -8,12 +9,14 @@ def encrypt():
     data = request.json
     text = data.get('text', '')
 
-    command = ['python3', 'NTRU.py', '-k', 'NTRU_key', '-eS', text, '-T']
-    
+    encoded_text = base64.b64encode(text.encode()).decode()
+
+    command = ['python3', 'NTRU.py', '-k', 'NTRU_key', '-eS', encoded_text, '-T']
+
     try:
         process = subprocess.run(command, check=True, stdout=subprocess.PIPE)
         output = process.stdout.decode('utf-8')
-        return jsonify({'result': output})
+        return jsonify({'result': output[:-1]})
     except subprocess.CalledProcessError as e:
         return jsonify({'error': f'ntru.py 실행 중 오류 발생: {e}'})
 
@@ -22,14 +25,17 @@ def decrypt():
     data = request.json
     text = data.get('text', '')
 
-    command = ['python3' 'NTRU.py', '-k', 'NTRU_key', '-T', '-dS', text,]
-    
+    command = ['python3', 'NTRU.py', '-k', 'NTRU_key', '-T', '-dS', text,]
+
     try:
         process = subprocess.run(command, check=True, stdout=subprocess.PIPE)
         output = process.stdout.decode('utf-8')
-        return jsonify({'result': output})
+
+        decoded_output = base64.b64decode(output.encode()).decode()
+
+        return jsonify({'result': decoded_output})
     except subprocess.CalledProcessError as e:
         return jsonify({'error': f'ntru.py 실행 중 오류 발생: {e}'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
